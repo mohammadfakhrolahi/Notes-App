f86ec74e366ad1a6383e86e9e6fe4def962bca54
Mohammad <fakhrolahi@gmail.com>
2024-01-01 11:31:52 +0330

    updated
    Updated BtnIcon.

--- src/components/buttons/BtnIcon.js
+++ src/components/buttons/BtnIcon.js
@@ -1,6 +1,6 @@
 const BtnIcon = (props) => {
   return (
-    <button className="w-11 h-11 p-2.5 rounded-full text-slate-500 hover:bg-slate-200 duration-200">
+    <button className="w-11 h-11 p-2.5 rounded-full text-slate-500 hover:bg-slate-200 active:bg-slate-300 duration-200">
       {props.children}
     </button>
   )
