a6fb7d1548fa15bf3bf200b19e183ea7d8c13469
Mohammad <fakhrolahi@gmail.com>
2024-01-01 13:16:06 +0330

    component
    Created Header component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
+import Logo from '../Logo/Logo'
+import TextInput from '../forms/TextInput'
+import BtnIcon from '../buttons/BtnIcon'
+
 const Header = () => {
   return (
-    <header className='container mx-auto'>
-      <span>Logo</span>
-      <div>
-        <form>
-          <input type="text" placeholder="Search" />
-        </form>
+    <header className="mx-4">
+      <div className="container flex justify-between items-center bg-white py-2 px-4 sm:px-10 mt-4  mx-auto rounded-full shadow-md">
+          <Logo/>
+          <div className="md:grow max-w-2xl">
+            <TextInput />
+          </div>
+          <div>
+            <BtnIcon>
+              <span class="material-symbols-rounded">settings</span>
+            </BtnIcon>
+          </div>
       </div>
     </header>
   )
