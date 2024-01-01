e862a1c33dd8d6a4ad951ac105c97861ac7f02ad
Mohammad <fakhrolahi@gmail.com>
2024-01-01 11:28:50 +0330

    component
    Created button for icons.

---
+++ src/components/buttons/BtnIcon.js
@@ -0,0 +1,9 @@
+const BtnIcon = (props) => {
+  return (
+    <button className="w-11 h-11 p-2.5 rounded-full text-slate-500 hover:bg-slate-200 duration-200">
+      {props.children}
+    </button>
+  )
+}
+
+export default BtnIcon
