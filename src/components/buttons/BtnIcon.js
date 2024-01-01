a20bf323f85719b9eda14ba11509dcddfbad4e59
Mohammad <fakhrolahi@gmail.com>
2024-01-01 12:11:59 +0330

    updated
    Added props and default class to className for BtnIcon.

--- src/components/buttons/BtnIcon.js
+++ src/components/buttons/BtnIcon.js
@@ -1,9 +1,7 @@
 const BtnIcon = (props) => {
-  return (
-    <button className="w-11 h-11 p-2.5 rounded-full text-slate-500 hover:bg-slate-200 active:bg-slate-300 duration-200">
-      {props.children}
-    </button>
-  )
+  const classes = `w-11 h-11 p-2.5 rounded-full text-slate-500 hover:bg-slate-200 active:bg-slate-300 duration-200 ${props.className} || ''`
+
+  return <button className={classes.trim()}>{props.children}</button>
 }
 
 export default BtnIcon
