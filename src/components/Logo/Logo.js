21880bd439eb1fa8e7e74bdff6e65520aa2df1b9
Mohammad <fakhrolahi@gmail.com>
2024-01-01 13:15:38 +0330

    component
    Created Logo component.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className="flex items-center ">
+      <span class="material-symbols-rounded text-amber-400">description</span>
+      <span className="text-slate-500 text-xl hidden sm:block">Notes</span>
+    </div>
+  )
+}
+
+export default Logo
