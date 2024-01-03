b20c52df32facc0568f036b64962d84475c3cf62
Mohammad <fakhrolahi@gmail.com>
2024-01-03 14:11:58 +0330

    component
    Created CardList component.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,10 @@ import React from 'react'
 import Button from '../buttons/Button'
 import Badge from '../Badge/Badge'
 
-const Card = ({ label, note, title }) => {
+const Card = ({ label, note, title, className }) => {
+  const classes = `p-4 rounded-3xl ${className} || ''`
   return (
-    <div className="bg-red-300 p-4 rounded-3xl">
+    <div className={classes}>
       <div>
         <div>
           <h4 className="pb-2 font-semibold">{title}</h4>
@@ -25,13 +26,13 @@ const Card = ({ label, note, title }) => {
           <span className="material-symbols-rounded">more_vert</span>
         </Button>
         <Button className="btn-xs">
-          <span class="material-symbols-rounded">palette</span>
-        </Button>
-        <Button className="btn-xs">
           <span class="material-symbols-rounded">archive</span>
         </Button>
         <Button className="btn-xs">
           <span class="material-symbols-rounded">delete</span>
+        </Button>
+        <Button className="btn-xs">
+          <span class="material-symbols-rounded">palette</span>
         </Button>
       </div>
     </div>
