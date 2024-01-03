6cf5347c1d0a593a8c1d29ca2da31903198cf19b
Mohammad <fakhrolahi@gmail.com>
2024-01-03 13:29:36 +0330

    component
    Created Card component for notes.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import Button from '../buttons/Button'
+import Badge from '../Badge/Badge'
+
+const Card = ({ label, note, title }) => {
+  return (
+    <div className="bg-red-300 p-4 rounded-3xl">
+      <div>
+        <div>
+          <h4 className="pb-2 font-semibold">{title}</h4>
+        </div>
+        <div>
+          <p className="pb-2">{note}</p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2 my-3">
+        <Badge>{label}</Badge>
+        <Badge>{label}</Badge>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Button className="btn-xs">
+          <span className="material-symbols-rounded">more_vert</span>
+        </Button>
+        <Button className="btn-xs">
+          <span class="material-symbols-rounded">palette</span>
+        </Button>
+        <Button className="btn-xs">
+          <span class="material-symbols-rounded">archive</span>
+        </Button>
+        <Button className="btn-xs">
+          <span class="material-symbols-rounded">delete</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
