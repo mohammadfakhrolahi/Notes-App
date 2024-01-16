653c9a45420a6e33036306ab7ee9c8b623e69805
Mohammad <fakhrolahi@gmail.com>
2024-01-16 15:42:16 +0330

    feat
    Added props to Button.

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,7 +1,7 @@
 const Button = (props) => {
   const classes = `duration-200 ${props.className} || ''`
 
-  return <button className={classes.trim()}>{props.children}</button>
+  return <button className={classes.trim()} onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button
