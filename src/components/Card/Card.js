a672e9bfaadf353250702aaa9ff46add6cf4869c
Mohammad <fakhrolahi@gmail.com>
2024-01-21 12:18:43 +0330

    style
    Updated Card component style.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,19 +4,21 @@ import Button from '../buttons/Button'
 import Badge from '../Badge/Badge'
 
 const Card = ({ note, title, color, children, className }) => {
-  const classes = `p-4 rounded-3xl ${color} ${className} || ''`
+  const classes = `flex flex-col justify-between p-4 rounded-3xl ${color} ${className} || ''`
   return (
     <div className={classes}>
       <div>
         <div>
-          <h4 className="pb-2 font-semibold">{title}</h4>
+          <div>
+            <h4 className="pb-2 font-semibold">{title}</h4>
+          </div>
+          <div>
+            <p className="pb-2">{note}</p>
+          </div>
         </div>
-        <div>
-          <p className="pb-2">{note}</p>
-        </div>
-      </div>
 
-      <div className="flex items-center gap-2 my-3">{children}</div>
+        <div className="flex items-center gap-2 my-3">{children}</div>
+      </div>
 
       <div className="flex items-center gap-2">
         <Button className="btn-xs">
