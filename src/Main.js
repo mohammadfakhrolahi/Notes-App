dc7e8e05f2dfd79234f96ba2c3b2ebae5e10e13c
Mohammad <fakhrolahi@gmail.com>
2024-02-03 09:16:10 +0330

    Updated.

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-import Container from './components/Container/Container'
 import AddNote from './components/forms/AddNote'
 import CardList from './components/CardList/CardList'
 
 const Main = () => {
   return (
-    <Container>
+    <main className="container px-4 mt-8 mx-auto">
       <section className="flex justify-center">
         <AddNote />
       </section>
       <section className="className='flex justify-center' mt-8">
         <CardList />
       </section>
-    </Container>
+    </main>
   )
 }
 
