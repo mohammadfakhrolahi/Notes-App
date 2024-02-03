810df57da72cbe7e57f0a99117f3f21ec4aadf6c
Mohammad <fakhrolahi@gmail.com>
2024-02-03 08:35:59 +0330

    feat
    Created Container component and used in Main component.

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
+import Container from './components/Container/Container'
 import AddNote from './components/forms/AddNote'
 import CardList from './components/CardList/CardList'
 
 const Main = () => {
   return (
-    <main className="container px-4 mt-8 mx-auto">
+    <Container>
       <section className="flex justify-center">
         <AddNote />
       </section>
       <section className="className='flex justify-center' mt-8">
         <CardList />
       </section>
-    </main>
+    </Container>
   )
 }
 
